2d685245fc3417aa268e67a33769daeb03b40cd4
Atta Ullah Saeed <attaullahsaeed77@gmail.com>
2020-08-26 13:40:20 +0500

    Updates

---
+++ precache-manifest.7974c5eac480df2744ec55916a2dac8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d68cad84041db9f62b4c90e8cce7b88",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "4ac2614eb09d88d9eb67",
+    "url": "/youtube-clone/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "7d2cdda9ee233c11af42",
+    "url": "/youtube-clone/static/css/main.8656c879.chunk.css"
+  },
+  {
+    "revision": "4ac2614eb09d88d9eb67",
+    "url": "/youtube-clone/static/js/2.87f8eb32.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/youtube-clone/static/js/2.87f8eb32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2cdda9ee233c11af42",
+    "url": "/youtube-clone/static/js/main.aa609fe1.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  },
+  {
+    "revision": "479ce7b894b31dee9b73c32210b6bd6f",
+    "url": "/youtube-clone/static/media/atta1.479ce7b8.jpg"
+  }
+]);
